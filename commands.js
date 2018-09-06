03b176832806ee42aaf69bb55c373ebabf34f7ad
shubham chitranshi <shubhamchitranshi@shubhams-MacBook-Pro.local>
2018-09-06 23:02:44 +0530

    Boiler plate code with commands used in the commands.js file

---
+++ commands.js
@@ -0,0 +1,12 @@
+// this project is called the chainlist
+// it is like a craiglist yellowpages to buy and sell products
+// there are different ways to initialize the project
+// like truffle init
+// this project requires to create a decentralized backend and a frontend
+// so we will use truffle boxes
+// this gives us a boilerplate project directory to build Dapps
+
+// bascially boxes are like boiler plate starter project
+// that you can use to get started . google truffle unbox
+// we are using the box that udemy created .
+truffle unbox chainskills/chainskills-box
