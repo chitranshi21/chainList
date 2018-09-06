1d4314be6ab7dbca54a8a46fd81fca60547a3f20
shubham chitranshi <shubhamchitranshi@shubhams-MacBook-Pro.local>
2018-09-06 23:19:27 +0530

    Added the ChainList Contract
    - define article variables
    - placeholder for getter and seller of article

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -1,5 +1,21 @@
 pragma solidity ^0.4.18;
 
+// first version - allow us to sell the article
+// by identifying the seller, article name, decripton and price
+// let us retrieve the article by returning its seller address, name
+// description and price
 contract ChainList {
-  
+  //define some state variables for the article
+
+  // type address represent an account, here seller
+  address seller;
+  string name;
+  string description;
+  // for price in crypto currency we use unsingned 256 int
+  unint256 price;
+
+  // to sell an article
+
+  // get an article
+
 }
