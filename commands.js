356906d1a05e7af8252bbab170ba638a9c71e922
chitranshi21 <chitranshi.shubham@gmail.com>
2018-09-07 15:42:08 +0530

    Added commands
    - To run contract on Ganache network
    - To call the get and sell method in the contract

--- commands.js
+++ commands.js
@@ -33,3 +33,15 @@ web3.fromWei(web3.eth.getBalance(web3.eth.accounts[0]), "ether").toNumber()
 //before calling any function from the contract, we have to get an instance of
 // it. the deployed function returns an instance of contract asyncronously.
 ChainList.deployed().then(function(instance) {app=instance});
+// or you can also write
+ChainList.deployed().then((instance) => app=instance);
+
+
+// now we can use the app variable to access the contract
+app.getArticle()
+// this returns all values either empty or 0. because there is no contructor
+// to initialize
+
+// to sell the article use following command
+app.sellArticle("iphone 7", "Selling this phone to buy iphone 8",
+                web3.toWei(3, "ether"), {from: web3.eth.accounts[1]});
