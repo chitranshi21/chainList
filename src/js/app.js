2ed6d507486421fe14cdb1ea9f8a3450bb23c78e
shubham chitranshi <shubhamchitranshi@shubhams-MacBook-Pro.local>
2018-09-16 12:20:40 +0530

    Front-end Code
    Added the CSS
    Edited the HTML to get the layout
    Edited App.js to load the data - Static for now.

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,19 @@ App = {
 
      init: function() {
           /*
-           * Replace me...
+           * Load Article Row here
            */
+
+          let articlesRow = $('#articlesRow');
+          let articleTemplate = $('#articleTemplate');
+
+          articleTemplate.find('.panel-title').text("Article 1");
+          articleTemplate.find('.article-description').text("Description for \
+          Article 1");
+          articleTemplate.find('.article-price').text('10.23');
+          articleTemplate.find('.article-seller').text('0x02232323209483409');
+
+          articlesRow.append(articleTemplate.html());
 
           return App.initWeb3();
      },
